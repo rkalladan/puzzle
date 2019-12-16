b8525484cb6e680f05d6d14051a9a5017589715a
Rahul Kalladan <rahul.kalladan@sap.com>
2019-12-16 19:26:12 +0530

    Hacker rank Electronic Shop problem

---
+++ src/main/java/com/learn/hr/ElectronicShop.java
@@ -0,0 +1,72 @@
+package com.learn.hr;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ElectronicShop {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] bnm = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int b = Integer.parseInt(bnm[0]);
+
+        int n = Integer.parseInt(bnm[1]);
+
+        int m = Integer.parseInt(bnm[2]);
+
+        int[] keyboards = new int[n];
+
+        String[] keyboardsItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        for (int keyboardsItr = 0; keyboardsItr < n; keyboardsItr++) {
+            int keyboardsItem = Integer.parseInt(keyboardsItems[keyboardsItr]);
+            keyboards[keyboardsItr] = keyboardsItem;
+        }
+
+        int[] drives = new int[m];
+
+        String[] drivesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        for (int drivesItr = 0; drivesItr < m; drivesItr++) {
+            int drivesItem = Integer.parseInt(drivesItems[drivesItr]);
+            drives[drivesItr] = drivesItem;
+        }
+
+        /*
+         * The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+         */
+
+        int moneySpent = getMoneySpent(keyboards, drives, b);
+
+//        bufferedWriter.write(String.valueOf(moneySpent));
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
+
+        scanner.close();
+    }
+
+	private static int getMoneySpent(int[] keyboards, int[] drives, int b) {
+		int max = -1;
+		if(b>=1 && b<= 1000000 && (1>=drives.length && 1>=keyboards.length) && (1000<=drives.length && 1000<=keyboards.length)) {
+			for(int i:keyboards) {
+				for(int j:drives) {
+					if((i+j>max) && (i+j < b)) {
+						max = i+j;
+					}
+				}
+			}
+		}
+		System.out.println(max);
+		return max;
+	}
+}
